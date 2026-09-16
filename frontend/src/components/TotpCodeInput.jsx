eea81c06bd74005d0b5d150a1946c17ad1fe74df
petrafan007 <petrafan007@users.noreply.github.com>
2026-09-15 21:22:20 -0400

    Fix Bitwarden and password manager autofill for TOTP fields (v3.0.19)

--- frontend/src/components/TotpCodeInput.jsx
+++ frontend/src/components/TotpCodeInput.jsx
@@ -45,11 +45,7 @@ const TotpCodeInput = forwardRef(function TotpCodeInput({ onChange, id = 'twoFac
       inputMode="numeric"
       pattern="[0-9]*"
       maxLength={6}
-      autoComplete="off"
-      data-bwignore="true"
-      data-1p-ignore="true"
-      data-lpignore="true"
-      data-form-type="other"
+      autoComplete="one-time-code"
       autoCapitalize="none"
       spellCheck={false}
       aria-label="Two-factor authentication code"
